e08ef30e0820a83d9eea71238baa17561a878b46
Arnie825 <arnietmp@gmail.com>
2020-04-29 21:29:44 -0500

    Added Project

---
+++ Chess/Assets/Scripts/StartScreenButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class StartScreenButton : MonoBehaviour
+{
+    // script to move onto the next sence when the button is pressed
+    public void ButtonClick()
+    {
+        SceneManager.LoadScene("Main Game");
+    }
+
+    public void ButtonClickQuit()
+    {
+        Application.Quit();
+    }
+}
+
